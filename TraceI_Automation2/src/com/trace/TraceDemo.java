d953192f818120b828da295672969618d4459c4e
Maruthi <maruthirajabn@gmail.com>
2022-02-20 13:37:37 +0530

    Initial commit....

---
+++ TraceI_Automation2/src/com/trace/TraceDemo.java
@@ -0,0 +1,15 @@
+package com.trace;
+
+public class TraceDemo {
+
+	public static void main(String[] args) 
+	{
+		browserLogin();
+	}
+	
+	static void browserLogin()
+	{
+		System.out.println("Browser Launched");
+	}
+
+}
